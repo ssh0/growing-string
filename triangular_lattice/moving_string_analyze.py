bbe6083a9d860d5fdf43d6161d46945b73f7609e
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-06-02 22:44:49 +0900

    moving_string_analyze.py: Modify

--- triangular_lattice/moving_string_analyze.py
+++ triangular_lattice/moving_string_analyze.py
@@ -9,8 +9,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-fig, ax = plt.subplots()
-pd.DataFrame(np.load(2016-05-31.npz))
+# Load data ===================================================================
+# data = np.load("2016-05-31.npz")
+data = np.load("2016-06-02.npz")
 
+# df = pd.DataFrame(data['T'], index=data['sizeset'])
+df = pd.DataFrame(1./data['T'], index=data['sizeset'])
+# print df.T
+df = df.T
 
+# box = df.plot.box(positions=data['sizeset'])
+box = df.plot.box(positions=data['sizeset'], showfliers=False, logx=True)
 
+box.set_title("Deadlock time for each string size")
+box.set_xlabel("String size $N$")
+# box.set_ylabel("Deadlock time $T$")
+box.set_ylabel("Inverse of deadlock time $1/T$")
+plt.show()
