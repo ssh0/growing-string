d604e78484f8df9cdbf609b2ef6e41cf46cdabe4
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-06-03 21:17:34 +0900

    Add new data 2016-06-03_120.npz

--- triangular_lattice/moving_string_analyze.py
+++ triangular_lattice/moving_string_analyze.py
@@ -13,6 +13,7 @@ import numpy as np
 # data = np.load("2016-05-31.npz")
 data = np.load("2016-06-02.npz")
 # data = np.load("2016-06-03_80.npz")
+# data = np.load("2016-06-03_120.npz")
 T = data['T']
 sizeset = data['sizeset']
 
@@ -38,15 +39,36 @@ fig, ax = plt.subplots()
 # [  8   9  10  11  12  13  14  15  16  17  18  20  21  23  24  26  28  30
 #   32  35  37  40  43  46  50  54  57  62  66  71  76  82  88  95 102 109
 #  117 126 135 145 156 167 179 192 207 222 238 256]
-n = 135
+n = 256
 if not n in sizeset:
     raise UserWarning("n is not in sizeset")
-df[n].plot.hist(ax=ax, bins=150, logy=True, ylim=(0, 1000))
-ax.set_title("Deadlock time distribution for string size $N = {}$".format(n))
+
+# df[n].plot.hist(ax=ax, bins=150, logy=True, ylim=(0, 1000))
+# ax.set_title("Deadlock time distribution for string size $N = {}$".format(n))
 
 # for n in [10, 32, 117, 256]:
 #     df[n].plot.hist(ax=ax, bins=150, logy=True, ylim=(0, 1000))
 # ax.set_title("Deadlock time distribution for string size $N$")
+
+# Lx=80のときと比較 ================
+# data2 = np.load("2016-06-03_80.npz")
+# T2 = data2['T']
+# sizeset2 = data2['sizeset']
+# df2 = pd.DataFrame(T2, index=sizeset2)
+# df2 = df2.T
+# df[n].plot.hist(ax=ax, bins=150, logy=True, ylim=(0, 1000), alpha=0.6)
+# df2[n].plot.hist(ax=ax, bins=150, logy=True, ylim=(0, 1000), alpha=0.6)
+# ==================================
+
+# Lx=120のときと比較 ================
+data3 = np.load("2016-06-03_120.npz")
+T3 = data3['T']
+sizeset3 = data3['sizeset']
+df3 = pd.DataFrame(T3, index=sizeset3)
+df3 = df3.T
+df[n].plot.hist(ax=ax, bins=150, logy=True, ylim=(0, 1000), alpha=0.6)
+df3[n].plot.hist(ax=ax, bins=150, logy=True, ylim=(0, 1000), alpha=0.6)
+# ==================================
 
 ax.set_xlabel("Deadlock time $T$")
 # ===================
