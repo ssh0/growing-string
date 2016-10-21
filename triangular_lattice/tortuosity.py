05628e6099510767a47ebaea77496564579aad95
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-10-21 15:03:05 +0900

    tortuosity: Create data and get the results

--- triangular_lattice/tortuosity.py
+++ triangular_lattice/tortuosity.py
@@ -24,7 +24,8 @@ def calc_tortuosity_for_each_beta(filename):
 
     y = np.array(distance_list)
     y = y.reshape((num_of_strings, int(y.shape[0] / num_of_strings/ num_of_pairs), num_of_pairs))
-    y = y / x
+    # tortuosity = length_of_the_curve / distance_betwee_the_ends_of_it
+    y = x / y
 
     y_ave = np.average(y, axis=2)
 
@@ -43,16 +44,37 @@ if __name__ == '__main__':
         '5-2': "./results/data/distances/beta=5.00_161015_153838.npz",
         '10-2': "./results/data/distances/beta=10.00_161015_154048.npz",
         '15-2': "./results/data/distances/beta=15.00_161015_154136.npz",
-        '20-2': "./results/data/distances/beta=20.00_161015_154419.npz"
+        '20-2': "./results/data/distances/beta=20.00_161015_154419.npz",
+        "0-3": "./results/data/distances/beta=0.00_161018_160133.npz",
+        "1-3": "./results/data/distances/beta=1.00_161018_161118.npz",
+        "2-3": "./results/data/distances/beta=2.00_161018_162849.npz",
+        "3-3": "./results/data/distances/beta=3.00_161018_164500.npz",
+        "4-3": "./results/data/distances/beta=4.00_161018_170824.npz",
+        "5-3": "./results/data/distances/beta=5.00_161018_172135.npz",
+        "6-3": "./results/data/distances/beta=6.00_161018_173918.npz",
+        "7-3": "./results/data/distances/beta=7.00_161018_175342.npz",
+        "8-3": "./results/data/distances/beta=8.00_161018_180914.npz",
+        "9-3": "./results/data/distances/beta=9.00_161018_182543.npz",
+        "10-3": "./results/data/distances/beta=10.00_161018_184136.npz",
+        "0-4": "./results/data/distances/beta=0.00_161018_202019.npz",
+        "5-4": "./results/data/distances/beta=5.00_161018_202404.npz",
+        "10-4": "./results/data/distances/beta=10.00_161018_202616.npz",
+        "15-4": "./results/data/distances/beta=15.00_161018_202846.npz",
+        "20-4": "./results/data/distances/beta=20.00_161018_203002.npz",
     }
     fig, ax = plt.subplots()
-    for i in [0, 5, 10, 15, 20]:
-        fn = result_data_path['%d-2' % i]
+    # for i in [0, 5, 10, 15, 20]:
+    for i in [0, 2, 4, 8, 10]:
+        # fn = result_data_path['%d-2' % i]
+        fn = result_data_path['%d-3' % i]
+        # fn = result_data_path['%d-4' % i]
         ax.plot(*calc_tortuosity_for_each_beta(fn),
                 ls='', marker='.', label=r'$\beta = %2.2f$' % float(i))
+    ax.plot(ax.get_xlim(), [1., 1.], 'k-')
+    ax.set_ylim(0., ax.get_ylim()[1])
     ax.set_xlabel(r'Path length $L$')
     ax.set_ylabel(r'Tortuosity $T$')
-    ax.set_title('Tortuosity $T = \lambda_{\mathrm{avg}} / L$')
+    ax.set_title('Tortuosity $T = L / \lambda_{\mathrm{avg}}$')
     ax.legend(loc='best')
     plt.show()
 
