8de6906af7aabc37a8e7181760dce20addffe214
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-12-21 20:24:33 +0900

    fractal_dim: Get distance data for some time span

--- triangular_lattice/fractal_dim.py
+++ triangular_lattice/fractal_dim.py
@@ -6,10 +6,12 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 from optimize import Optimize_powerlaw
 
 
-def calc_tortuosity_for_each_beta(ax, i, filename):
+def calc_fractal_dim_for_each_beta(ax, i, filename):
     data = np.load(filename)
     beta = data['beta']
     num_of_strings = data['num_of_strings']
@@ -81,13 +83,14 @@ if __name__ == '__main__':
         #     "./results/data/distances/frames=120_beta=8.00_161208_143310.npz",
         #     "./results/data/distances/frames=120_beta=10.00_161208_143329.npz",
         # ],
+
         150: [
             "./results/data/distances/frames=150_beta=0.00_161208_210328.npz",
-            "./results/data/distances/frames=150_beta=10.00_161208_210407.npz",
             "./results/data/distances/frames=150_beta=2.00_161208_210345.npz",
             "./results/data/distances/frames=150_beta=4.00_161208_210350.npz",
             "./results/data/distances/frames=150_beta=6.00_161208_210354.npz",
             "./results/data/distances/frames=150_beta=8.00_161208_210400.npz",
+            "./results/data/distances/frames=150_beta=10.00_161208_210407.npz",
         ],
         300: [
             "./results/data/distances/frames=300_beta=0.00_161208_143342.npz",
@@ -144,7 +147,152 @@ if __name__ == '__main__':
             "./results/data/distances/frames=1200_beta=6.00_161208_163345.npz",
             "./results/data/distances/frames=1200_beta=8.00_161208_163842.npz",
             "./results/data/distances/frames=1200_beta=10.00_161208_164024.npz",
-        ]
+        ],
+        1350: [
+            "./results/data/distances/frames=1350_beta=0.00_161219_163059.npz",
+            "./results/data/distances/frames=1350_beta=2.00_161219_163229.npz",
+            "./results/data/distances/frames=1350_beta=4.00_161219_200509.npz",
+            "./results/data/distances/frames=1350_beta=6.00_161219_230504.npz",
+            "./results/data/distances/frames=1350_beta=8.00_161220_020451.npz",
+            "./results/data/distances/frames=1350_beta=10.00_161220_050348.npz",
+        ],
+        1500: [
+            "./results/data/distances/frames=1500_beta=0.00_161219_170034.npz",
+            "./results/data/distances/frames=1500_beta=2.00_161219_170406.npz",
+            "./results/data/distances/frames=1500_beta=4.00_161219_203520.npz",
+            "./results/data/distances/frames=1500_beta=6.00_161219_233306.npz",
+            "./results/data/distances/frames=1500_beta=8.00_161220_023318.npz",
+            "./results/data/distances/frames=1500_beta=10.00_161220_053252.npz",
+        ],
+        1650: [
+            "./results/data/distances/frames=1650_beta=0.00_161219_173703.npz",
+            "./results/data/distances/frames=1650_beta=2.00_161219_174118.npz",
+            "./results/data/distances/frames=1650_beta=4.00_161219_211624.npz",
+            "./results/data/distances/frames=1650_beta=6.00_161220_000711.npz",
+            "./results/data/distances/frames=1650_beta=8.00_161220_030835.npz",
+            "./results/data/distances/frames=1650_beta=10.00_161220_061122.npz",
+        ],
+        1800: [
+            "./results/data/distances/frames=1800_beta=0.00_161219_181458.npz",
+            "./results/data/distances/frames=1800_beta=2.00_161219_182205.npz",
+            "./results/data/distances/frames=1800_beta=4.00_161219_215624.npz",
+            "./results/data/distances/frames=1800_beta=6.00_161220_005219.npz",
+            "./results/data/distances/frames=1800_beta=8.00_161220_035046.npz",
+            "./results/data/distances/frames=1800_beta=10.00_161220_065509.npz",
+        ],
+        1950: [
+            "./results/data/distances/frames=1950_beta=0.00_161219_185939.npz",
+            "./results/data/distances/frames=1950_beta=2.00_161219_190733.npz",
+            "./results/data/distances/frames=1950_beta=4.00_161219_224207.npz",
+            "./results/data/distances/frames=1950_beta=6.00_161220_014202.npz",
+            "./results/data/distances/frames=1950_beta=8.00_161220_044049.npz",
+            "./results/data/distances/frames=1950_beta=10.00_161220_074620.npz",
+        ],
+
+        # 150: [
+        #     "./results/data/distances/frames=150_beta=0.00_161220_144021.npz",
+        #     "./results/data/distances/frames=150_beta=2.00_161220_174953.npz",
+        #     "./results/data/distances/frames=150_beta=4.00_161220_212543.npz",
+        #     "./results/data/distances/frames=150_beta=6.00_161221_013534.npz",
+        #     "./results/data/distances/frames=150_beta=8.00_161221_053006.npz",
+        #     "./results/data/distances/frames=150_beta=10.00_161221_092909.npz",
+        # ],
+        # 300: [
+        #     "./results/data/distances/frames=300_beta=0.00_161220_144115.npz",
+        #     "./results/data/distances/frames=300_beta=2.00_161220_175052.npz",
+        #     "./results/data/distances/frames=300_beta=4.00_161220_212654.npz",
+        #     "./results/data/distances/frames=300_beta=6.00_161221_013639.npz",
+        #     "./results/data/distances/frames=300_beta=8.00_161221_053108.npz",
+        #     "./results/data/distances/frames=300_beta=10.00_161221_093013.npz",
+        # ],
+        # 450: [
+        #     "./results/data/distances/frames=450_beta=0.00_161220_144320.npz",
+        #     "./results/data/distances/frames=450_beta=2.00_161220_175307.npz",
+        #     "./results/data/distances/frames=450_beta=4.00_161220_212927.npz",
+        #     "./results/data/distances/frames=450_beta=6.00_161221_013901.npz",
+        #     "./results/data/distances/frames=450_beta=8.00_161221_053332.npz",
+        #     "./results/data/distances/frames=450_beta=10.00_161221_093236.npz",
+        # ],
+        # 600: [
+        #     "./results/data/distances/frames=600_beta=0.00_161220_144707.npz",
+        #     "./results/data/distances/frames=600_beta=2.00_161220_175701.npz",
+        #     "./results/data/distances/frames=600_beta=4.00_161220_213353.npz",
+        #     "./results/data/distances/frames=600_beta=6.00_161221_014314.npz",
+        #     "./results/data/distances/frames=600_beta=8.00_161221_053748.npz",
+        #     "./results/data/distances/frames=600_beta=10.00_161221_093657.npz",
+        # ],
+        # 750: [
+        #     "./results/data/distances/frames=750_beta=0.00_161220_145258.npz",
+        #     "./results/data/distances/frames=750_beta=2.00_161220_180302.npz",
+        #     "./results/data/distances/frames=750_beta=4.00_161220_214108.npz",
+        #     "./results/data/distances/frames=750_beta=6.00_161221_015024.npz",
+        #     "./results/data/distances/frames=750_beta=8.00_161221_054453.npz",
+        #     "./results/data/distances/frames=750_beta=10.00_161221_094356.npz",
+        # ],
+        # 900: [
+        #     "./results/data/distances/frames=900_beta=0.00_161220_150120.npz",
+        #     "./results/data/distances/frames=900_beta=2.00_161220_181200.npz",
+        #     "./results/data/distances/frames=900_beta=4.00_161220_215230.npz",
+        #     "./results/data/distances/frames=900_beta=6.00_161221_020013.npz",
+        #     "./results/data/distances/frames=900_beta=8.00_161221_055443.npz",
+        #     "./results/data/distances/frames=900_beta=10.00_161221_095400.npz",
+        # ],
+        # 1050: [
+        #     "./results/data/distances/frames=1050_beta=0.00_161220_151234.npz",
+        #     "./results/data/distances/frames=1050_beta=2.00_161220_182408.npz",
+        #     "./results/data/distances/frames=1050_beta=4.00_161220_220804.npz",
+        #     "./results/data/distances/frames=1050_beta=6.00_161221_021404.npz",
+        #     "./results/data/distances/frames=1050_beta=8.00_161221_060827.npz",
+        #     "./results/data/distances/frames=1050_beta=10.00_161221_100741.npz",
+        # ],
+        # 1200: [
+        #     "./results/data/distances/frames=1200_beta=0.00_161220_152914.npz",
+        #     "./results/data/distances/frames=1200_beta=2.00_161220_183941.npz",
+        #     "./results/data/distances/frames=1200_beta=4.00_161220_222724.npz",
+        #     "./results/data/distances/frames=1200_beta=6.00_161221_023130.npz",
+        #     "./results/data/distances/frames=1200_beta=8.00_161221_062652.npz",
+        #     "./results/data/distances/frames=1200_beta=10.00_161221_102628.npz",
+        # ],
+        # 1350: [
+        #     "./results/data/distances/frames=1350_beta=0.00_161220_154801.npz",
+        #     "./results/data/distances/frames=1350_beta=2.00_161220_190107.npz",
+        #     "./results/data/distances/frames=1350_beta=4.00_161220_224925.npz",
+        #     "./results/data/distances/frames=1350_beta=6.00_161221_025428.npz",
+        #     "./results/data/distances/frames=1350_beta=8.00_161221_065034.npz",
+        #     "./results/data/distances/frames=1350_beta=10.00_161221_105035.npz",
+        # ],
+        # 1500: [
+        #     "./results/data/distances/frames=1500_beta=0.00_161220_161154.npz",
+        #     "./results/data/distances/frames=1500_beta=2.00_161220_192658.npz",
+        #     "./results/data/distances/frames=1500_beta=4.00_161220_232018.npz",
+        #     "./results/data/distances/frames=1500_beta=6.00_161221_032233.npz",
+        #     "./results/data/distances/frames=1500_beta=8.00_161221_071933.npz",
+        #     "./results/data/distances/frames=1500_beta=10.00_161221_112018.npz",
+        # ],
+        # 1650: [
+        #     "./results/data/distances/frames=1650_beta=0.00_161220_163934.npz",
+        #     "./results/data/distances/frames=1650_beta=2.00_161220_195922.npz",
+        #     "./results/data/distances/frames=1650_beta=4.00_161221_000037.npz",
+        #     "./results/data/distances/frames=1650_beta=6.00_161221_035826.npz",
+        #     "./results/data/distances/frames=1650_beta=8.00_161221_075528.npz",
+        #     "./results/data/distances/frames=1650_beta=10.00_161221_115541.npz",
+        # ],
+        # 1800: [
+        #     "./results/data/distances/frames=1800_beta=0.00_161220_171202.npz",
+        #     "./results/data/distances/frames=1800_beta=2.00_161220_203633.npz",
+        #     "./results/data/distances/frames=1800_beta=4.00_161221_004752.npz",
+        #     "./results/data/distances/frames=1800_beta=6.00_161221_044012.npz",
+        #     "./results/data/distances/frames=1800_beta=8.00_161221_083736.npz",
+        #     "./results/data/distances/frames=1800_beta=10.00_161221_123947.npz",
+        # ],
+        # 1950: [
+        #     "./results/data/distances/frames=1950_beta=0.00_161220_174934.npz",
+        #     "./results/data/distances/frames=1950_beta=2.00_161220_212524.npz",
+        #     "./results/data/distances/frames=1950_beta=4.00_161221_013517.npz",
+        #     "./results/data/distances/frames=1950_beta=6.00_161221_052949.npz",
+        #     "./results/data/distances/frames=1950_beta=8.00_161221_092852.npz",
+        #     "./results/data/distances/frames=1950_beta=10.00_161221_133606.npz",
+        # ]
     }
 
 
@@ -159,7 +307,7 @@ if __name__ == '__main__':
             # fn = result_data_path['%d-2' % i]
             fn = result_data_path['%d-3' % i]
             # fn = result_data_path['%d-4' % i]
-            beta, D = calc_tortuosity_for_each_beta(ax1, i, fn)
+            beta, D = calc_fractal_dim_for_each_beta(ax1, i, fn)
             betas.append(beta)
             Ds.append(D)
 
@@ -184,7 +332,7 @@ if __name__ == '__main__':
             fig, ax1 = plt.subplots()
             betas, Ds = [], []
             for i, f in enumerate(distance_data[frames]):
-                beta, D = calc_tortuosity_for_each_beta(ax1, i, f)
+                beta, D = calc_fractal_dim_for_each_beta(ax1, i, f)
                 betas.append(beta)
                 Ds.append(D)
 
@@ -229,10 +377,26 @@ if __name__ == '__main__':
         T = np.array(T)
         D = np.array(D_T).T
         betas = np.array(betas)
+
+        # X, Y = np.meshgrid(betas, T)
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111, projection='3d')
+
+        # ax.plot_wireframe(X, Y, D.T, rstride=1)
+        # ax.set_zlim(1., ax.get_zlim()[1])
+        # ax.set_title('Fractal dimension')
+        # ax.set_xlabel(r'$\beta$')
+        # ax.set_ylabel(r'$T$')
+        # ax.set_zlabel(r'$D(T)$')
+        # plt.show()
+
         fig, ax = plt.subplots()
-        for beta, D_beta in zip(betas, D):
-            ax.plot(T, D_beta, ls='', marker='.',
+        for i, (beta, D_beta) in enumerate(zip(betas, D)):
+            color = cm.viridis(float(i) / (len(betas) - 1))
+            ax.plot(T, D_beta, ls='', marker='o',
                     label=r'$\beta = %2.2f$' % beta,
+                    color=color,
+                    markeredgewidth=0.0
                     )
 
         ax.set_xlim(0, ax.get_xlim()[1] + 100)
@@ -244,4 +408,4 @@ if __name__ == '__main__':
         plt.show()
 
     # fractal_dims()
-    variation_of_fd(plot=False, save_image=True)
+    variation_of_fd(plot=False, save_image=False)
