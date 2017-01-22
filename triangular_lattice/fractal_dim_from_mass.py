8112796b817d9b14570e6ffd6419b0f34e2d578f
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2017-01-22 23:26:05 +0900

    fractal_dim_from_mass: Modify file path specifying

--- triangular_lattice/fractal_dim_from_mass.py
+++ triangular_lattice/fractal_dim_from_mass.py
@@ -36,7 +36,7 @@ def _plot_data_for_validation(paths, raw=False):
             beta, num_of_strings, N_r, frames, r, M = load_data(path)
             r, M = averaging_data(r, M, N_r, scale='log')
             ax.loglog(r, M, '.',
-                      label=r'$\beta = %2.2f$, $T = %d' % (beta, frames))
+                      label=r'$\beta = %2.2f$, $T = %d$' % (beta, frames))
         ax.set_title('Averaged mass in the circle of radius $r$')
         ax.set_ylabel(r'Averaged mass in the circle of radius')
     ax.legend(loc='best')
@@ -105,7 +105,7 @@ def get_fractal_dim(path):
 
         if event.key == 'x':
             # save image
-            fn = "results/img/mass_in_r/frames=%d_beta=%2.2f" % (frames, beta)
+            fn = "./results/img/mass_in_r/frames=%d_beta=%2.2f" % (frames, beta)
             fn += "_" + time.strftime("%y%m%d_%H%M%S") + ".png"
             plt.savefig(fn)
             print "[saved] " + fn
@@ -115,7 +115,7 @@ def get_fractal_dim(path):
     fig.canvas.mpl_connect('key_press_event', press)
     plt.show()
 
-def get_paths(fix=None, beta_num=0, frame_num=0):
+def get_paths(fix=None, beta_num=0, frame_num=0, ver=1):
     """get specific condtion datas
     filter: 'beta' or 'frames'
     """
@@ -125,73 +125,144 @@ def get_paths(fix=None, beta_num=0, frame_num=0):
     # ls -1 ./results/data/mass_in_r/beta=6.00_frames=*.npz | sort -V
     # ls -1 ./results/data/mass_in_r/beta=8.00_frames=*.npz | sort -V
     # ls -1 ./results/data/mass_in_r/beta=10.00_frames=*.npz | sort -V
-    result_data_paths = [
-        "./results/data/mass_in_r/beta=0.00_frames=200_170122_014239.npz",
-        "./results/data/mass_in_r/beta=0.00_frames=400_170122_014239.npz",
-        "./results/data/mass_in_r/beta=0.00_frames=600_170122_014240.npz",
-        "./results/data/mass_in_r/beta=0.00_frames=800_170122_014240.npz",
-        "./results/data/mass_in_r/beta=0.00_frames=1000_170122_014240.npz",
-        "./results/data/mass_in_r/beta=0.00_frames=1200_170122_014240.npz",
-        "./results/data/mass_in_r/beta=0.00_frames=1400_170122_014240.npz",
-        "./results/data/mass_in_r/beta=0.00_frames=1600_170122_014240.npz",
-        "./results/data/mass_in_r/beta=0.00_frames=1800_170122_014240.npz",
-        "./results/data/mass_in_r/beta=0.00_frames=2000_170122_014240.npz",
 
-        "./results/data/mass_in_r/beta=2.00_frames=200_170122_015933.npz",
-        "./results/data/mass_in_r/beta=2.00_frames=400_170122_015933.npz",
-        "./results/data/mass_in_r/beta=2.00_frames=600_170122_015933.npz",
-        "./results/data/mass_in_r/beta=2.00_frames=800_170122_015933.npz",
-        "./results/data/mass_in_r/beta=2.00_frames=1000_170122_015933.npz",
-        "./results/data/mass_in_r/beta=2.00_frames=1200_170122_015933.npz",
-        "./results/data/mass_in_r/beta=2.00_frames=1400_170122_015933.npz",
-        "./results/data/mass_in_r/beta=2.00_frames=1600_170122_015933.npz",
-        "./results/data/mass_in_r/beta=2.00_frames=1800_170122_015933.npz",
-        "./results/data/mass_in_r/beta=2.00_frames=2000_170122_015933.npz",
+    if ver == 0:
+        result_data_paths = [
+            "./results/data/mass_in_r/beta=0.00_frames=200_170122_014239.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=400_170122_014239.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=600_170122_014240.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=800_170122_014240.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1000_170122_014240.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1200_170122_014240.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1400_170122_014240.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1600_170122_014240.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1800_170122_014240.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=2000_170122_014240.npz",
 
-        "./results/data/mass_in_r/beta=4.00_frames=200_170122_023211.npz",
-        "./results/data/mass_in_r/beta=4.00_frames=400_170122_023211.npz",
-        "./results/data/mass_in_r/beta=4.00_frames=600_170122_023211.npz",
-        "./results/data/mass_in_r/beta=4.00_frames=800_170122_023211.npz",
-        "./results/data/mass_in_r/beta=4.00_frames=1000_170122_023211.npz",
-        "./results/data/mass_in_r/beta=4.00_frames=1200_170122_023211.npz",
-        "./results/data/mass_in_r/beta=4.00_frames=1400_170122_023211.npz",
-        "./results/data/mass_in_r/beta=4.00_frames=1600_170122_023211.npz",
-        "./results/data/mass_in_r/beta=4.00_frames=1800_170122_023211.npz",
-        "./results/data/mass_in_r/beta=4.00_frames=2000_170122_023211.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=200_170122_015933.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=400_170122_015933.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=600_170122_015933.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=800_170122_015933.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1000_170122_015933.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1200_170122_015933.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1400_170122_015933.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1600_170122_015933.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1800_170122_015933.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=2000_170122_015933.npz",
 
-        "./results/data/mass_in_r/beta=6.00_frames=200_170122_025607.npz",
-        "./results/data/mass_in_r/beta=6.00_frames=400_170122_025607.npz",
-        "./results/data/mass_in_r/beta=6.00_frames=600_170122_025607.npz",
-        "./results/data/mass_in_r/beta=6.00_frames=800_170122_025607.npz",
-        "./results/data/mass_in_r/beta=6.00_frames=1000_170122_025607.npz",
-        "./results/data/mass_in_r/beta=6.00_frames=1200_170122_025607.npz",
-        "./results/data/mass_in_r/beta=6.00_frames=1400_170122_025607.npz",
-        "./results/data/mass_in_r/beta=6.00_frames=1600_170122_025607.npz",
-        "./results/data/mass_in_r/beta=6.00_frames=1800_170122_025607.npz",
-        "./results/data/mass_in_r/beta=6.00_frames=2000_170122_025607.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=200_170122_023211.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=400_170122_023211.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=600_170122_023211.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=800_170122_023211.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1000_170122_023211.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1200_170122_023211.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1400_170122_023211.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1600_170122_023211.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1800_170122_023211.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=2000_170122_023211.npz",
 
-        "./results/data/mass_in_r/beta=8.00_frames=200_170122_032301.npz",
-        "./results/data/mass_in_r/beta=8.00_frames=400_170122_032301.npz",
-        "./results/data/mass_in_r/beta=8.00_frames=600_170122_032301.npz",
-        "./results/data/mass_in_r/beta=8.00_frames=800_170122_032301.npz",
-        "./results/data/mass_in_r/beta=8.00_frames=1000_170122_032301.npz",
-        "./results/data/mass_in_r/beta=8.00_frames=1200_170122_032301.npz",
-        "./results/data/mass_in_r/beta=8.00_frames=1400_170122_032301.npz",
-        "./results/data/mass_in_r/beta=8.00_frames=1600_170122_032301.npz",
-        "./results/data/mass_in_r/beta=8.00_frames=1800_170122_032301.npz",
-        "./results/data/mass_in_r/beta=8.00_frames=2000_170122_032301.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=200_170122_025607.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=400_170122_025607.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=600_170122_025607.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=800_170122_025607.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1000_170122_025607.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1200_170122_025607.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1400_170122_025607.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1600_170122_025607.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1800_170122_025607.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=2000_170122_025607.npz",
 
-        "./results/data/mass_in_r/beta=10.00_frames=200_170122_033138.npz",
-        "./results/data/mass_in_r/beta=10.00_frames=400_170122_033138.npz",
-        "./results/data/mass_in_r/beta=10.00_frames=600_170122_033138.npz",
-        "./results/data/mass_in_r/beta=10.00_frames=800_170122_033138.npz",
-        "./results/data/mass_in_r/beta=10.00_frames=1000_170122_033138.npz",
-        "./results/data/mass_in_r/beta=10.00_frames=1200_170122_033138.npz",
-        "./results/data/mass_in_r/beta=10.00_frames=1400_170122_033138.npz",
-        "./results/data/mass_in_r/beta=10.00_frames=1600_170122_033138.npz",
-        "./results/data/mass_in_r/beta=10.00_frames=1800_170122_033138.npz",
-        "./results/data/mass_in_r/beta=10.00_frames=2000_170122_033138.npz",
-    ]
+            "./results/data/mass_in_r/beta=8.00_frames=200_170122_032301.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=400_170122_032301.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=600_170122_032301.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=800_170122_032301.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1000_170122_032301.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1200_170122_032301.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1400_170122_032301.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1600_170122_032301.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1800_170122_032301.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=2000_170122_032301.npz",
+
+            "./results/data/mass_in_r/beta=10.00_frames=200_170122_033138.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=400_170122_033138.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=600_170122_033138.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=800_170122_033138.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1000_170122_033138.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1200_170122_033138.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1400_170122_033138.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1600_170122_033138.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1800_170122_033138.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=2000_170122_033138.npz",
+        ]
+
+    if ver == 1:
+        result_data_paths = [
+            "./results/data/mass_in_r/beta=0.00_frames=200_170122_200327.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=400_170122_200327.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=600_170122_200327.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=800_170122_200327.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1000_170122_200327.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1200_170122_200327.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1400_170122_200327.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1600_170122_200327.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=1800_170122_200327.npz",
+            "./results/data/mass_in_r/beta=0.00_frames=2000_170122_200327.npz",
+
+            "./results/data/mass_in_r/beta=2.00_frames=200_170122_201957.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=400_170122_201957.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=600_170122_201957.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=800_170122_201957.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1000_170122_201957.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1200_170122_201957.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1400_170122_201957.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1600_170122_201957.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=1800_170122_201957.npz",
+            "./results/data/mass_in_r/beta=2.00_frames=2000_170122_201957.npz",
+
+            "./results/data/mass_in_r/beta=4.00_frames=200_170122_210816.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=400_170122_210816.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=600_170122_210816.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=800_170122_210816.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1000_170122_210816.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1200_170122_210816.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1400_170122_210816.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1600_170122_210816.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=1800_170122_210816.npz",
+            "./results/data/mass_in_r/beta=4.00_frames=2000_170122_210816.npz",
+
+            "./results/data/mass_in_r/beta=6.00_frames=200_170122_213938.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=400_170122_213938.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=600_170122_213938.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=800_170122_213938.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1000_170122_213938.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1200_170122_213938.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1400_170122_213938.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1600_170122_213938.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=1800_170122_213938.npz",
+            "./results/data/mass_in_r/beta=6.00_frames=2000_170122_213938.npz",
+
+            "./results/data/mass_in_r/beta=8.00_frames=200_170122_221218.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=400_170122_221218.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=600_170122_221218.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=800_170122_221218.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1000_170122_221218.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1200_170122_221218.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1400_170122_221218.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1600_170122_221218.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=1800_170122_221218.npz",
+            "./results/data/mass_in_r/beta=8.00_frames=2000_170122_221218.npz",
+
+            "./results/data/mass_in_r/beta=10.00_frames=200_170122_224351.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=400_170122_224351.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=600_170122_224351.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=800_170122_224351.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1000_170122_224351.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1200_170122_224351.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1400_170122_224351.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1600_170122_224351.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=1800_170122_224351.npz",
+            "./results/data/mass_in_r/beta=10.00_frames=2000_170122_224351.npz",
+        ]
 
     if fix == 'beta':  # fix beta (all frames)
         result_data_paths = [result_data_paths[beta_num * 10 + i]
@@ -211,8 +282,9 @@ if __name__ == '__main__':
     ##         0  1  2  3  4  5
 
     # result_data_paths = get_paths(beta_num=5, frame_num=9)
-    # result_data_paths = get_paths(fix='frames', frame_num=7)
-    # result_data_paths = get_paths(fix='beta', beta_num=0)
+    # result_data_paths = get_paths(fix='frames', frame_num=7, ver=0)
+
+    result_data_paths = get_paths(fix='beta', beta_num=5, ver=1)
 
     # _plot_data_for_validation(result_data_paths, raw=True)
     # _plot_data_for_validation(result_data_paths)
